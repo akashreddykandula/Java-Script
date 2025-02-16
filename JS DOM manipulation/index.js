9a58ec534b25754b8309d0112197b3d95394dd8b
Akash Reddy <akashreddykandula@gmail.com>
2025-02-16 15:19:46 +0530

    Add files via upload

---
+++ JS DOM manipulation/index.js
@@ -0,0 +1,12 @@
+let mydiv = document.querySelector('#mydiv');
+
+// let newElement=document.createElement('span');
+
+// newElement.textContent="akash reddy kandula"
+
+// mydiv.insertAdjacentElement('beforebegin',newElement);
+
+let parent= document.querySelector('#mydiv');
+
+let child=document.querySelector('#fpara');
+parent.removeChild(child);
